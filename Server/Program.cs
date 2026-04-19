fd78f9cc248474eaeab3ce82d684a39321ab537e
vobotomo <vobornyt@gmail.com>
2026-04-19 15:31:23 +0200

    feat: JSON nacitani sveta (I1)

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,47 +5,9 @@ namespace Server
         public static void Main(string[] args)
         {
             var world = new WorldManager();
-
             var npcManager = new NpcManager();
 
-            npcManager.AddNpc(new Npc
-            {
-                Name = "Technik Orion",
-                Dialog = "Hele, reaktor je nestabilni. Potrebuju klic od skladu abych ho opravil. Videl jsem ho nekde tady..."
-            });
-
-            npcManager.AddNpc(new Npc
-            {
-                Name = "Kapitan",
-                Dialog = "Stanice je ztracena. Jedina sance je dostat se do hangaru a aktivovat uknikovy modul."
-            });
-            
-            world.AddRoom(new Room
-            {
-                Id = 1,
-                Name = "Zachranna kapsle",
-                Description = "Rozbite svetlo, kovovy zapach. Tady jsi se probudil.",
-                Exits = new System.Collections.Generic.Dictionary<string, int> { { "sever", 2 } },
-                Items = new System.Collections.Generic.List<string> { "zapisnik" }
-            });
-
-            world.AddRoom(new Room
-            {
-                Id = 2,
-                Name = "Chodba A",
-                Description = "Polotemna chodba s kabelazi.",
-                Exits = new System.Collections.Generic.Dictionary<string, int> { { "jih", 1 }, { "sever", 3 } }
-            });
-
-            world.AddRoom(new Room
-            {
-                Id = 3,
-                Name = "Reaktorova hala",
-                Description = "Obrovska mistnost humici chladicimi ventilatory.",
-                Exits = new System.Collections.Generic.Dictionary<string, int> { { "jih", 2 } },
-                Items = new System.Collections.Generic.List<string> { "klic od skladu" },
-                Npcs = new System.Collections.Generic.List<string> { "Technik Orion" }
-            });
+            JsonLoader.LoadWorld("world.json", world, npcManager);
 
             var server = new TcpServer(4000, world, npcManager);
             server.StartAsync().GetAwaiter().GetResult();
--- Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2a8a9337e7e9886d27ff0fd7f03cd2ec094855f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd507bc42cef1fb9e3b76a4ac6c42562ea38b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
