d2a8a9337e7e9886d27ff0fd7f03cd2ec094855f
vobotomo <vobornyt@gmail.com>
2026-04-19 15:05:39 +0200

    feat: inventar, vezmi, poloz

--- Server/CommandProcessor.cs
+++ Server/CommandProcessor.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Server
 {
@@ -25,6 +24,9 @@ namespace Server
             {
                 "prozkoumej" => Prozkoumej(player),
                 "jdi"        => Jdi(player, argument),
+                "vezmi"      => Vezmi(player, argument),
+                "poloz"      => Poloz(player, argument),
+                "inventar"   => Inventar(player),
                 "pomoc"      => Pomoc(),
                 _            => "Neznam tento prikaz. Pis 'pomoc' pro seznam prikazu."
             };
@@ -54,13 +56,65 @@ namespace Server
             return "Smer '" + smer + "' tady neexistuje.";
         }
 
+        private string Vezmi(Player player, string predmet)
+        {
+            if (string.IsNullOrWhiteSpace(predmet))
+                return "Co chces vzit? Napr: vezmi zapisnik";
+
+            var room = _world.GetRoom(player.CurrentRoomId);
+            if (room == null) return "Mistnost nenalezena.";
+
+            string found = room.Items.Find(i => i.ToLower() == predmet.ToLower());
+            if (found == null)
+                return "Predmet '" + predmet + "' tady neni.";
+
+            if (player.Inventory.Count >= player.MaxCapacity)
+                return "Inventar je plny! Maximalni kapacita: " + player.MaxCapacity;
+
+            room.Items.Remove(found);
+            player.Inventory.Add(found);
+            return "Vzal jsi: " + found;
+        }
+
+        private string Poloz(Player player, string predmet)
+        {
+            if (string.IsNullOrWhiteSpace(predmet))
+                return "Co chces polozit? Napr: poloz zapisnik";
+
+            string found = player.Inventory.Find(i => i.ToLower() == predmet.ToLower());
+            if (found == null)
+                return "'" + predmet + "' nemas v inventari.";
+
+            var room = _world.GetRoom(player.CurrentRoomId);
+            if (room == null) return "Mistnost nenalezena.";
+
+            player.Inventory.Remove(found);
+            room.Items.Add(found);
+            return "Polozil jsi: " + found;
+        }
+
+        private string Inventar(Player player)
+        {
+            if (player.Inventory.Count == 0)
+                return "Inventar je prazdny. (kapacita: " + player.MaxCapacity + ")";
+
+            var sb = new System.Text.StringBuilder();
+            sb.AppendLine("Inventar (" + player.Inventory.Count + "/" + player.MaxCapacity + "):");
+            foreach (var item in player.Inventory)
+                sb.AppendLine("  - " + item);
+            return sb.ToString();
+        }
+
         private string Pomoc()
         {
             return
                 "\nSEZNAM PRIKAZU:\n\n" +
-                "  prozkoumej    - zobrazi aktualni mistnost\n" +
-                "  jdi <smer>    - pohyb, napr: jdi sever\n" +
-                "  pomoc         - zobrazi tento seznam\n";
+                "  prozkoumej        - zobrazi aktualni mistnost\n" +
+                "  jdi <smer>        - pohyb, napr: jdi sever\n" +
+                "  vezmi <predmet>   - vezmi predmet z mistnosti\n" +
+                "  poloz <predmet>   - poloz predmet z inventare\n" +
+                "  inventar          - zobrazi inventar\n" +
+                "  pomoc             - zobrazi tento seznam\n";
         }
     }
 }
--- Server/Player.cs
+++ Server/Player.cs
@@ -1,8 +1,12 @@
-﻿namespace Server
+﻿using System.Collections.Generic;
+
+namespace Server
 {
     public class Player
     {
         public string Name { get; set; }
         public int CurrentRoomId { get; set; } = 1;
+        public List<string> Inventory { get; set; } = new List<string>();
+        public int MaxCapacity { get; set; } = 5;
     }
 }
